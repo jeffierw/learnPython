8d647ec08c8e20d36c9b0adfdb41f874b400eb30
jeffierw <13324545303@163.com>
2018-12-24 22:40:04 +0800

    Day1 finished

---
+++ pythonExample/ex-5.py
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+my_height = height * 2.54 # centimeters
+weight = 180.0 # lbs
+my_weight = weight * 0.4536 # kilos
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+print "Let's talk about %s." % name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+age, my_height, my_weight, age + my_height + my_weight)
